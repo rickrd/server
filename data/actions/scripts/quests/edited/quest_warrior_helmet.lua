46b44327335fe999499f8039ecd6f3c039aa0a36
rickarof <kryonz_ricardo@hotmail.com>
2016-09-23 22:20:11 -0300

    xx

--- data/actions/scripts/quests/edited/quest_warrior_helmet.lua
+++ data/actions/scripts/quests/edited/quest_warrior_helmet.lua
@@ -1,14 +1,14 @@
-function onUse(cid, item, frompos, item2, topos)
-  	-- 10cc Quest
+function onUse(cid, item)
+  	-- warrior helmet Quest
   	if item.aid == 45001 then
 		local itemWeight = getItemWeightById(item.uid, 1)
 		local playerCap = getPlayerFreeCap(cid)
 		recompensa = 2475
-		if getPlayerStorageValue(cid, item.uid) == -1 then
+		if getPlayerStorageValue(cid, 15001) == -1 then
 			if playerCap >= itemWeight then
 				doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, 'You have found a ' .. getItemNameById(recompensa) .. '.')
 				doPlayerAddItem(cid, recompensa ,1)
-				setPlayerStorageValue(cid, item.uid, 1)
+				setPlayerStorageValue(cid, 15001, 1)
 			else
 				doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, 'You have found a ' .. getItemNameById(recompensa) .. ' weighing ' .. itemWeight .. ' oz it\'s too heavy.')
 			end
